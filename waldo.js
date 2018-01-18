4884c7ae17f8a2c7bede21434da7f1f62dfb938d
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-01-18 16:34:53 +0000

    final commit with forEach

--- waldo.js
+++ waldo.js
@@ -1,14 +1,15 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  // for (var i = 0; i < arr.length; i++) {
+    arr.forEach(function(name, index) {
+    if (name === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  })
 }
 
-function actionWhenFound(i) {
-  console.log("Found Waldo at Index " + i + " !");
+function actionWhenFound(indexHere) {
+  console.log("Found Waldo at Index " + indexHere + " !");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
